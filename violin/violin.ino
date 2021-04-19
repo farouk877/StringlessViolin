2f01d173d70c0e0c62e655972059aafed252773f
Naman Bhargava <namanb.edu@gmail.com>
2021-04-19 19:46:44 -0400

    Fixed progression error

--- violin/violin.ino
+++ violin/violin.ino
@@ -40,13 +40,13 @@ int speakerpin = 4;
 // Repeat notes fill in nonexistent BS and ES gaps (EASIER TO PLAY)
 int melody[8][4] = {
                     {NOTE_G3,  NOTE_D4,  NOTE_A4,  NOTE_E5},
-                    {NOTE_GS3, NOTE_DS4, NOTE_AS4, NOTE_F5},
-                    {NOTE_A3,  NOTE_E4,  NOTE_B4,  NOTE_FS5},
-                    {NOTE_AS3, NOTE_E4,  NOTE_B4,  NOTE_G5},
-                    {NOTE_B3,  NOTE_F4,  NOTE_C5,  NOTE_GS5},
-                    {NOTE_B3,  NOTE_FS4, NOTE_CS5, NOTE_A5},
-                    {NOTE_C4,  NOTE_G4,  NOTE_D5,  NOTE_AS5},
-                    {NOTE_CS4, NOTE_GS4, NOTE_DS5, NOTE_B6}
+                    {NOTE_GS3, NOTE_DS4, NOTE_AS4, NOTE_E5},
+                    {NOTE_A3,  NOTE_E4,  NOTE_B4,  NOTE_F5},
+                    {NOTE_AS3, NOTE_E4,  NOTE_B4,  NOTE_FS5},
+                    {NOTE_B3,  NOTE_F4,  NOTE_C5,  NOTE_G5},
+                    {NOTE_B3,  NOTE_FS4, NOTE_CS5, NOTE_GS5},
+                    {NOTE_C4,  NOTE_G4,  NOTE_D5,  NOTE_A5},
+                    {NOTE_CS4, NOTE_GS4, NOTE_DS5, NOTE_AS5}
 //                    {NOTE_D4,  NOTE_A4,  NOTE_E5,  NOTE_B6}
                    };
 
